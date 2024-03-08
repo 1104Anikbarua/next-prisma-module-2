2abce7d1fc334ee5c13a78367997865e4924305a
Anik Barua <anikbb05@gmail.com>
2024-03-09 02:05:40 +0600

    create user profile

--- src/create.ts
+++ src/create.ts
@@ -2,15 +2,26 @@ import { PrismaClient, Role } from "@prisma/client";
 
 const prisma = new PrismaClient();
 // create user
+// const main = async () => {
+//   const createUser = await prisma.user.create({
+//     data: {
+//       userName: "user_3",
+//       email: "user3@gmail.com",
+//       role: Role.user,
+//     },
+//   });
+//   console.log(createUser);
+// };
+
+// main();
+
 const main = async () => {
-  const createUser = await prisma.user.create({
+  const createProfile = await prisma.profile.create({
     data: {
-      userName: "user_3",
-      email: "user3@gmail.com",
-      role: Role.user,
+      bio: "This is user one bio",
+      userId: 1,
     },
   });
-  console.log(createUser);
 };
 
 main();
