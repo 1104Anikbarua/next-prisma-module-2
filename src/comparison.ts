532bc2cbae919fb982c58e45a3ec236b22a82eeb
Anik Barua <anikbb05@gmail.com>
2024-03-09 20:01:45 +0600

    filter post with and,or & contains,equals

---
+++ src/comparison.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const mainAnd = async () => {
+  const findPosts = await prisma.post.findMany({
+    where: {
+      AND: [
+        {
+          title: {
+            contains: "title-1",
+            // mode: "insensitive",
+          },
+        },
+        {
+          published: {
+            equals: true,
+          },
+        },
+      ],
+    },
+  });
+  console.log(findPosts);
+};
+
+// mainAnd();
+
+const main = async () => {
+  const updatePost = await prisma.post.update({
+    where: {
+      id: 2,
+    },
+    data: {
+      title: "This is title-1",
+      published: true,
+    },
+  });
+};
+// main();
+
+const mainOr = async () => {
+  const filterPosts = await prisma.post.findMany({
+    where: {
+      OR: [
+        {
+          title: {
+            contains: "title-3",
+          },
+        },
+        {
+          title: {
+            contains: "title-2",
+          },
+        },
+      ],
+    },
+  });
+  console.log(filterPosts);
+};
+
+mainOr();
